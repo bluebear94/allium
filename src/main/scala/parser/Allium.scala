ca34bc5f0fc78bb63e4180c5363109a1c6b55e75
Tae Lim Kook <tkook11@gmail.com>
2014-05-31 22:55:34 -0400

    Added even more functions

--- src/main/scala/parser/Allium.scala
+++ src/main/scala/parser/Allium.scala
@@ -1,6 +1,6 @@
 package parser
 
-import scala.math.BigInt
+import scala.math._
 import scala.collection.mutable.{ Stack, HashMap }
 import scala.util.parsing.input._
 import scala.util.parsing.combinator._
@@ -160,8 +160,78 @@ object Allium {
         case (Real(a), Real(b)) => Boolean(a <= b)
         case (AString(a), AString(b)) => Boolean(a <= b)
       })
+    }),
+    ("to", (s: Stack[Type], e: HashMap[String, Type]) => {
+      val e1 = s.pop.toString.toLowerCase
+      val e0 = s.pop
+      s.push((e0, e1) match {
+        case (Integer(a), "integer") => e0
+        case (Integer(a), "real") => Real(a.toDouble)
+        case (Integer(a), "string") => AString(a.toString)
+        case (Real(a), "integer") => Integer(BigDecimal(a).toBigInt)
+        case (Real(a), "real") => e0
+        case (Real(a), "string") => AString(a.toString)
+        case (AString(a), "integer") => Integer(BigInt(a))
+        case (AString(a), "real") => Real(a.toDouble)
+        case (AString(a), "string") => e0
+      })
+    }),
+    ("dup", (s: Stack[Type], e: HashMap[String, Type]) => {
+      s.push(s.top)
+    }),
+    ("rem", (s: Stack[Type], e: HashMap[String, Type]) => {
+      s.pop()
+    }),
+    ("tp", (s: Stack[Type], e: HashMap[String, Type]) => {
+      val e1 = s.pop
+      val e0 = s.pop
+      s.push(e1)
+      s.push(e0)
+    }),
+    ("&&", (s: Stack[Type], e: HashMap[String, Type]) => {
+      val e1 = s.pop
+      val e0 = s.pop
+      s.push(Boolean(toBool(e0) && toBool(e1)))
+    }),
+    ("||", (s: Stack[Type], e: HashMap[String, Type]) => {
+      val e1 = s.pop
+      val e0 = s.pop
+      s.push(Boolean(toBool(e0) || toBool(e1)))
+    }),
+    ("^^", (s: Stack[Type], e: HashMap[String, Type]) => {
+      val e1 = s.pop
+      val e0 = s.pop
+      s.push(Boolean(toBool(e0) ^ toBool(e1)))
+    }),
+    ("&", (s: Stack[Type], e: HashMap[String, Type]) => {
+      val e1 = s.pop
+      val e0 = s.pop
+      s.push((e0, e1) match {
+        case (Integer(a), Integer(b)) => Integer(a & b)
+      })
+    }),
+    ("|", (s: Stack[Type], e: HashMap[String, Type]) => {
+      val e1 = s.pop
+      val e0 = s.pop
+      s.push((e0, e1) match {
+        case (Integer(a), Integer(b)) => Integer(a | b)
+      })
+    }),
+    ("b^", (s: Stack[Type], e: HashMap[String, Type]) => {
+      val e1 = s.pop
+      val e0 = s.pop
+      s.push((e0, e1) match {
+        case (Integer(a), Integer(b)) => Integer(a ^ b)
+      })
     })
   )
   def bti(b: Boolean) = if (b) 1 else 0
   def Boolean(b: Boolean) = Integer(bti(b))
+  def toBool(t: Type) = t match {
+    case Integer(n) => n != 0
+    case Real(x) => x != 0
+    case AString(s) => s != ""
+    case AnArray(a) => !a.isEmpty
+    case _ => true
+  }
 }
